8b274582d0235366ef5f6972edf24e0ba470fc06
Giang <nguyenvietgiang1110@gmail.com>
2023-09-01 10:50:03 +0700

    Tạo CSDL SQLite

--- Baitaplon/home/admin.py
+++ Baitaplon/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import DictionaryEntry
 
-# Register your models here.
+@admin.register(DictionaryEntry)
+class DictionaryEntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'english', 'vietnam', 'category', 'pronounce')
+    search_fields = ('english', 'vietnam', 'category', 'pronounce')
+
--- Baitaplon/home/models.py
+++ Baitaplon/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class DictionaryEntry(models.Model):
+    id = models.AutoField(primary_key=True)  
+    english = models.CharField(max_length=100)
+    vietnam = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    image = models.CharField(max_length=200)
+    pronounce = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.id}: {self.english}"
